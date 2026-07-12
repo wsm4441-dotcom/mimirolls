947b1a026b3513456ca377db61c2f9ab11d4af74
wsm <wsm@alMacBook-Air-alkhas-bwsm.local>
2026-07-12 03:46:17 +0300

    Set favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "mimirolls🍥",
   description: "ميمي رولز طازجة داخل جدة",
+  icons: {
+    icon: "/favicon.ico",
+  },
 };
 
 export default function RootLayout({
